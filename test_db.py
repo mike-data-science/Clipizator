f129638e28764ca77d352c666786d0427cb3407d
Developer Mike <gamedevelopermike@gmail.com>
2026-08-23 02:03:20 +0300

    Clean up and fix deployment

--- test_db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3, os
-db = os.path.expanduser('~/.publikclip/db.sqlite3')
-conn = sqlite3.connect(db)
-c = conn.cursor()
-c.execute("SELECT status, error FROM jobs WHERE id='20260820-172550-6ff85a'")
-print(c.fetchone())
